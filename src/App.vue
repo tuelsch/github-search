4fa19a12400692298a9cea0705f107d2f08fa32f
Philipp Gfeller <tuelsch@gmail.com>
2019-11-09 13:03:47 +0100

    Demo App

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Github User Search</h1>
+    <p>A technical assignment from Ginetta for applicants. To be solved in under 3h.</p>
+    <user-search />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import UserSearch from "./components/UserSearch.vue";
+
+@Component({
+  components: {
+    UserSearch
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
